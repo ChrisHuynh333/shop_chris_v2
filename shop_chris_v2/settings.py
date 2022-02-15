1bb229bc896ed07d2251fa6a5b3ee322ac43af18
Chris Huynh <chris.huynh333@gmail.com>
2022-02-15 17:19:57 -0600

    Revert "Add db entries and images"
    This reverts commit 33d9a4bd213227a0d60278576061ea737bfa4f2c.

--- shop_chris_v2/settings.py
+++ shop_chris_v2/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shop',
-    'shop_admin'
+    'shop_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
