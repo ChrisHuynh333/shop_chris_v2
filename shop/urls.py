daf28e320b97084bd1a6d11ab18b934f5930b76a
ChrisHuynh333 <chris.huynh333@gmail.com>
2022-02-16 12:55:43 -0600

    Update views.py and urls.py. Add nav to navbar in base.html

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,11 @@ app_name = "shop"
 urlpatterns = [
     path("", views.index, name="index"),
     path("product/<str:product>", views.product, name="product"),
+    path("category/<str:category>", views.category, name="category"),
+    path("all-products", views.all_products, name="all_products"),
+    path("account", views.account, name="account"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("cart", views.cart, name="cart"),
+    path("order-complete", views.order_complete, name="order_complete")
 ]
--- shop/views.py
+++ shop/views.py
@@ -6,6 +6,29 @@ def index(request):
     return render(request, "shop/index.html")
 
 def product(request, product):
-    return render(request, "shop/product.html", {
+    return render(request, "shop/products_individual.html", {
         "product": product
     })
+
+def category(request, category):
+    return render(request, "shop/products_by_category.html", {
+        "category": category
+    })
+
+def all_products(request):
+    return render(request, "shop/all_products.html")
+
+def account(request):
+    return render(request, "shop/account.html")
+
+def login(request):
+    return render(request, "shop/login.html")
+
+def register(request):
+    return render(request, "shop/register.html")
+
+def cart(request):
+    return render(request, "shop/cart.html")
+
+def order_complete(request):
+    return render(request, "shop/order_complete.html")
